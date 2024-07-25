f7506563350ba39bcb0277905e992ef012659c4a
neuro <yamsh@yandex.ru>
2024-07-25 11:00:04 +0500

    read content of the index html and put it into the src file as a string

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,6 @@
 from bs4 import BeautifulSoup
+
+# read content of the index html and put it into the src file as a string
+with open("scrap_tutorial/lesson1/blank/index.html") as file:
+    src = file.read()
+print(src)
