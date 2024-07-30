5ff6fb596d0d3d6c884c783d4b28e09224933052
neuro <yamsh@yandex.ru>
2024-07-30 18:00:11 +0500

    introducing methods '.next_element' '.previouse_element' '.find_next()'

--- lesson1.py
+++ lesson1.py
@@ -25,9 +25,22 @@ print(user_info_spans) """
 #print(user_info_spans[2].text)
 """ social_links = soup.find('div', {'class': 'social__networks'}).find('ul').find_all('a')
 print(social_links) """
-all_a = soup.find_all('a')
+""" all_a = soup.find_all('a')
 print(all_a)
 
 for item in all_a:
+    item_text = item.text
     item_url = item.get('href')
-    print(item_url)
+    print(f'{item_text}: {item_url}') """
+# .find_parent() .find_parents()
+""" post_div = soup.find(class_='post__text').find_parent('div', 'user__post')
+print(post_div) """
+# post_divs = soup.find(class_='post__text').find_parents('div', 'user__post__info')
+# print(post_divs)
+# .next_element .previous_element
+""" div_next = soup.find('div', {'class' : 'post__title'}).next_element.next_element\
+.text
+print(div_next)
+div_find_next = soup.find('div', {'class' : 'post__title'}).find_next().text
+print(div_find_next) """
+# find_next_sibling() .find_previouse_sibling()
